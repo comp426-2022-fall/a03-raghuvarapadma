381e2222b9ac102770ddbcd33ba7af5b8bae5009
Raghuvara Padma <raghuvarapadma@gmail.com>
2022-11-15 17:28:20 -0500

    base cases added

--- bin/cli.js
+++ bin/cli.js
@@ -5,5 +5,17 @@ import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2));
 
+if (!("sides" in args)) {
+	args.sides = 6;
+}
+
+if (!("dice" in args)) {
+	args.dice = 2;
+}
+
+if (!("rolls" in args)) {
+	args.rolls = 1;
+}
+
 console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
 
